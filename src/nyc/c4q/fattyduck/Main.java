4b7d9ed6726acea1a6372019f6f58c443ea55bf5
fattyduck <dison.ruan@yahoo.com>
2015-03-07 15:06:12 -0500

    first exercise madlib

--- src/nyc/c4q/fattyduck/Main.java
+++ src/nyc/c4q/fattyduck/Main.java
@@ -1,8 +1,30 @@
 package nyc.c4q.fattyduck;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("good morning silly");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What kind of animal are you?");
+        String aniRace;
+        Double price;
+        aniRace=input.nextLine();
+
+        if(aniRace.equals ("duck")){
+            price=1.25;
+            System.out.print("Dear " + aniRace + ", the cost of your ticket is " +price+ " mesos");
+
+        }else if(aniRace.equals("panda")){
+            price=2.5;
+            System.out.print("Dear " + aniRace + ", the cost of your ticket is " +price+ " mesos");
+        }else if (aniRace.equals("human")){
+            price=115.99;
+            System.out.print("Dear " + aniRace + ", the cost of your ticket is " +price+ " mesos");
+        }else{
+            System.out.println("Get out of here");
+        }
+
     }
 }
